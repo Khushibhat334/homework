be7df45b4a7bbca7a347408c90c819f80d4d1700
Khushi Bhat <146862503+Khushibhat334@users.noreply.github.com>
2023-11-20 00:06:02 +0530

    Add files via upload

---
+++ program of calculator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+int x;
+printf("1=Addition 2=Subtraction 3=Multiplication 4=Division");
+scanf("%d",&x);
+if(x==1)
+{
+c=a+b;
+printf("Addition:%d",c);
+}
+else if(x==2)
+{
+c=a-b;
+printf("Subtraction:%d",c);
+}
+else if(x==3)
+{
+c=a*b;
+printf("Multiplication:%d",c);
+}
+else if(x==4)
+{
+if(b==0)
+{
+printf("Infinity");
+}
+else
+{
+c=a/b;
+printf("Division:%d",c);
+}
+}
+}
