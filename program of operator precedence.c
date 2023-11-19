c53b47ed8a79574e509e2709c7f0759d8b508d07
Khushi Bhat <146862503+Khushibhat334@users.noreply.github.com>
2023-11-19 23:59:41 +0530

    Add files via upload

---
+++ program of operator precedence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a=5,b=10,c=15;
+int result1,result2,result3,result4;
+result1=a+b*c;
+result2=(a+b)*c;
+result3=a-b/c;
+result4=(a-b)/c;
+printf("%d\n",result1);
+printf("%d\n",result2);
+printf("%d\n",result3);
+printf("%d\n",result4);
+}
